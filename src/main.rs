5825169d0d878ee81886180b56dd40ae5831f94d
Justin Dubs <jtdubs@gmail.com>
2021-12-07 14:59:58 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate clap;
-use clap::{App, Arg};
-use std::io::{Read, Write, stdin, stdout};
+
+use std::io::{Read, Write, BufReader, BufWriter, stdin, stdout};
 use std::fs::{File};
+use clap::{App, Arg};
 
 fn main() -> Result<(), std::io::Error> {
     let matches =
@@ -24,34 +25,37 @@ fn main() -> Result<(), std::io::Error> {
                 .help("Wrap encoded lines after COLS character (default 76).  Use 0 to disable line wrapping")
                 .takes_value(true)
                 .default_value("76")
-                .validator(|arg| { arg.parse::<u32>().and(Ok(())).or(Err("wrap value must be a number".to_string())) }))
+                .validator(|arg| { arg.parse::<usize>().and(Ok(())).or(Err("wrap value must be a number".to_string())) }))
             .arg(Arg::with_name("FILE"))
             .get_matches();
 
     let _decode         = matches.is_present("decode");
     let _ignore_garbage = matches.is_present("ignore_garbage");
-    let _wrap_column    = matches.value_of("wrap").unwrap().parse::<u32>().unwrap_or(76);
+    let wrap_column    = matches.value_of("wrap").unwrap().parse::<usize>().ok().filter(|&x| x != 0);
     let file           = matches.value_of("FILE").unwrap_or("-");
+
+    let stdout = stdout();
+    let stdout_handle = stdout.lock();
+    let writer = BufWriter::new(stdout_handle);
 
     if file == "-" {
         let stdin = stdin();
-        let stdin_handle = stdin.lock();
-        b64_encode(stdin_handle)
+        let stdin_lock = stdin.lock();
+        b64_encode(BufReader::new(stdin_lock), writer, wrap_column)
     } else {
-        b64_encode(File::open(file).unwrap())
+        let file_handle = File::open(file)?;
+        b64_encode(BufReader::new(file_handle), writer, wrap_column)
     }
 }
 
-fn b64_encode(mut reader: impl Read) -> Result<(), std::io::Error> {
+fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>) -> Result<(), std::io::Error> {
     let alphabet : Vec<u8> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".bytes().collect();
 
-    let stdout = stdout();
-    let mut stdout_handle = stdout.lock();
-
-    let mut read_buffer  : [u8; 65536]  = [0; 65536];
-    let mut read_index   : usize        = 0;
-    let mut write_buffer : [u8; 131072] = [0; 131072];
-    let mut write_index  : usize        = 0;
+    let mut read_buffer  : [u8; 65536] = [0; 65536];
+    let mut read_index   : usize       = 0;
+    let mut write_buffer : [u8; 98304] = [0; 98304];
+    let mut write_index  : usize       = 0;
+    let mut current_col  : usize       = 0;
 
     loop {
         // fill buffer
@@ -86,8 +90,8 @@ fn b64_encode(mut reader: impl Read) -> Result<(), std::io::Error> {
         // update read index to end of risidual data
         read_index %= 3;
 
-        // write base64 characters to stdout
-        stdout_handle.write_all(&write_buffer[0..write_index])?;
+        // output base64 characters
+        current_col = wrapping_write(&write_buffer, write_index, wrap, current_col, &mut writer)?;
         write_index = 0;
     }
 
@@ -115,7 +119,29 @@ fn b64_encode(mut reader: impl Read) -> Result<(), std::io::Error> {
         _ => { unreachable!("impossible mod 3 value"); }
     }
 
-    stdout_handle.write_all(&write_buffer[0..write_index])?;
+    let _ = wrapping_write(&write_buffer, write_index, wrap, current_col, &mut writer)?;
 
     Ok(())
+}
+
+fn wrapping_write(buffer: &[u8], len: usize, wrap_col: Option<usize>, mut current_col: usize, writer: &mut impl Write) -> Result<usize, std::io::Error> {
+    if let Some(line_length) = wrap_col {
+        let mut written = 0;
+        while written < len {
+            let line_remaining = line_length - current_col;
+            let byte_remaining = len - written;
+            let n = line_remaining.min(byte_remaining);
+            writer.write_all(&buffer[written..written+n])?;
+            written += n;
+            if n == line_remaining {
+                writer.write_all(b"\n")?;
+                current_col = 0 as usize;
+            } else {
+                current_col += n;
+            }
+        }
+    } else {
+        writer.write_all(&buffer[0..len])?;
+    }
+    Ok(current_col)
 }
