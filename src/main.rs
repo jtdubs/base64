4881f59cdbe6f7545d5d7cdce318a1d599cdd747
Justin Dubs <jtdubs@gmail.com>
2021-12-07 15:08:07 -0500

    docs and cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs::{File};
 use clap::{App, Arg};
 
 fn main() -> Result<(), std::io::Error> {
+    // parse command line arguments
     let matches =
         App::new("base64")
             .version("0.0.1")
@@ -29,27 +30,36 @@ fn main() -> Result<(), std::io::Error> {
             .arg(Arg::with_name("FILE"))
             .get_matches();
 
+    // pull out arguments
     let decode         = matches.is_present("decode");
     let ignore_garbage = matches.is_present("ignore_garbage");
     let wrap_column    = matches.value_of("wrap").unwrap().parse::<usize>().ok().filter(|&x| x != 0);
     let file           = matches.value_of("FILE").unwrap_or("-");
 
+    // writer is always stdout
     let stdout = stdout();
     let stdout_handle = stdout.lock();
     let writer = BufWriter::new(stdout_handle);
 
+    // reader can be stdin or a file
     if file == "-" {
+        // make a buffered reader around stdin
         let stdin = stdin();
         let stdin_lock = stdin.lock();
         let reader = BufReader::new(stdin_lock);
+
+        // encode or decode as requested
         if decode {
             b64_decode(reader, writer, ignore_garbage)
         } else {
             b64_encode(reader, writer, wrap_column)
         }
     } else {
+        // make a buffered reader around the file
         let file_handle = File::open(file)?;
         let reader = BufReader::new(file_handle);
+
+        // encode or decode as requested
         if decode {
             b64_decode(reader, writer, ignore_garbage)
         } else {
@@ -72,7 +82,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
     let mut current_col  : usize       = 0;
 
     loop {
-        // fill buffer
+        // fill read buffer
         let bytes_read = reader.read(&mut read_buffer[read_index..])?;
 
         // if out of data, exit loop
@@ -83,7 +93,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
         // update read_index base on bytes_read
         read_index += bytes_read;
 
-        // process all chunks of 3 bytes into 4 base64 characters
+        // process all chunks of 3 bytes into 4 output characters
         for chunk in read_buffer[0..read_index].chunks_exact(3) {
             let (a, b, c) = (chunk[0], chunk[1], chunk[2]);
             write_buffer[write_index]   = alphabet[(a >> 2)                      as usize];
@@ -93,7 +103,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
             write_index += 4;
         }
 
-        // move risidual data to front of buffer
+        // move residual data to front of buffer
         match read_index % 3 {
             0 => { }
             1 => { read_buffer[0] = read_buffer[read_index-1]; }
@@ -101,7 +111,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
             _ => { unreachable!("impossible mod 3 value"); }
         }
 
-        // update read index to end of risidual data
+        // update read index to end of residual data
         read_index %= 3;
 
         // output base64 characters
@@ -113,6 +123,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
     match read_index % 3 {
         0 => { }
         1 => {
+            // output last byte as two data chars and two padding chars
             let a = read_buffer[0];
             write_buffer[write_index]   = alphabet[(a >> 2)         as usize];
             write_buffer[write_index+1] = alphabet[((a & 0x3) << 4) as usize];
@@ -122,6 +133,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
             write_index += 5;
         }
         2 => {
+            // output last two byte as three data chars and one padding char
             let (a, b) = (read_buffer[0], read_buffer[1]);
             write_buffer[write_index]   = alphabet[(a >> 2)                      as usize];
             write_buffer[write_index+1] = alphabet[(((a & 0x3) << 4) | (b >> 4)) as usize];
@@ -133,6 +145,7 @@ fn b64_encode(mut reader: impl Read, mut writer: impl Write, wrap: Option<usize>
         _ => { unreachable!("impossible mod 3 value"); }
     }
 
+    // output base64 characters
     let _ = wrapping_write(&write_buffer, write_index, wrap, current_col, &mut writer)?;
 
     Ok(())
